3b4376565f5b1b91cc82734d9be6f0b324d7fe59
unknown <joaovictor_gon@hotmail.com>
2025-12-05 23:45:39 -0300

    SellerDao interface

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void Insert(Department obj);
+	void Update(Department obj);
+	void DeleteById(Integer id);
+	Department FindById(Integer id);
+	List<Department> findAll();
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	void Insert(Seller obj);
+	void Update(Seller obj);
+	void DeleteById(Integer id);
+	Seller FindById(Integer id);
+	List<Seller> findAll();
+}
